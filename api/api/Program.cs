a011beb90534d2ccbdb23ab80572bfe3e5327538
harounatiambou00 <harounatiambou.pro@gmail.com>
2022-09-29 14:26:24 +0100

    Setting up dbup-sqlserver

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -1,7 +1,30 @@
+using DbUp;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+//This gets the database connection from the appsettings.json file and creates the database if it doesn't exist.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+/**
+ * Setting up dbup-sqlserver and upgrade
+*/
+var dbupUpgrader = DeployChanges.To.SqlDatabase(connectionString, null)
+                                    .WithScriptsEmbeddedInAssembly(
+                                        System.Reflection.Assembly.GetExecutingAssembly()
+                                        )
+                                    .WithTransaction()
+                                    .Build();
+// check whether there are any pending SQL Scripts, and using the PerformUpgrade method to do the actual migration.
+if (dbupUpgrader.IsUpgradeRequired())
+{
+    dbupUpgrader.PerformUpgrade();
+}
+
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
